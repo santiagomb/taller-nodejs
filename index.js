3a805080e84f74b655464f49077766b4be5b2589
santiagomb <santiago.m92@hotmail.com>
2016-12-01 13:38:10 -0300

    Modificado el listen. Establecer punto 'start' en package

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var http = require("http");
 var server = http.createServer();
 
 server.on("request", function(req, res){
-	//res.end("Hola, mundo");
 	var date = new Date();
 	res.end(JSON.stringify(date));
 });
 
-server.listen(3000);
+server.listen(procces.env.PORT || 3000);
