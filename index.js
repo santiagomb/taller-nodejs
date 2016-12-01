f3eae9cd6733ab45b9463a52fe5f5ab91056d0a5
santiagomb <santiago.m92@hotmail.com>
2016-12-01 13:55:53 -0300

    Probando URL de Heroku

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var http = require("http");
+var url = require("url");
 
 var server = http.createServer();
 
 server.on("request", function(req, res){
-	var date = new Date();
-	res.end(JSON.stringify(date));
+	var urlData = url.parse(req.url, true);
+	//var date = new Date();
+	//res.end(JSON.stringify(date));
+	
+	// poner en browser localhost:3000/recurso/sub?id=1) y ver el json que retorna
+	res.end(JSON.stringify(urlData));
 });
 
 server.listen(process.env.PORT || 3000);
